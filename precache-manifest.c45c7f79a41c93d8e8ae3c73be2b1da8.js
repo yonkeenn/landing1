1e7d546716cdc6425eaad5657c8d930b786f45ed
YonCloud <jhon.vargasr@gmail.com>
2020-03-03 17:51:18 -0500

    Updates

---
+++ precache-manifest.c45c7f79a41c93d8e8ae3c73be2b1da8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d266c0b858ee48a7b73149e047cf029",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd4606976962682abf5",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "92c0c9329b90715e130f",
+    "url": "/static/css/main.b9689649.chunk.css"
+  },
+  {
+    "revision": "0bd4606976962682abf5",
+    "url": "/static/js/2.2fede773.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2fede773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c0c9329b90715e130f",
+    "url": "/static/js/main.0447a406.chunk.js"
+  },
+  {
+    "revision": "c4d9e53ed168ab97b2d4",
+    "url": "/static/js/runtime-main.46894f80.js"
+  },
+  {
+    "revision": "41a4272ee454d951d4ce0833c41c3c94",
+    "url": "/static/media/complementos.41a4272e.jpg"
+  },
+  {
+    "revision": "33dc0b0d3a6ddd2bcc3136321d0bd4dc",
+    "url": "/static/media/fondo.33dc0b0d.jpg"
+  },
+  {
+    "revision": "dfd862880b63b5076247b2531e548f2e",
+    "url": "/static/media/fondo1.dfd86288.jpg"
+  },
+  {
+    "revision": "c815ee0b69eaa225dc0a3093c7bfc611",
+    "url": "/static/media/mesa2.c815ee0b.png"
+  },
+  {
+    "revision": "bfa8252cbc5b2e9bd84c9da12cdfec65",
+    "url": "/static/media/planta1.bfa8252c.jpg"
+  },
+  {
+    "revision": "95e02083a6a4913126eb4c295f2e6a49",
+    "url": "/static/media/ramo21.95e02083.jpg"
+  },
+  {
+    "revision": "33abd1271d360e4aabe98b1b17a7052f",
+    "url": "/static/media/text777.33abd127.jpg"
+  },
+  {
+    "revision": "73e6fe3d2a59b4f8dbd2bf5122045431",
+    "url": "/static/media/text869.73e6fe3d.png"
+  }
+]);
